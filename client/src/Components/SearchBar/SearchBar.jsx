85fccae53a23316d76ffc3520e7814d783d5e5d8
EzequielZarcos <ezequielzarcos159@gmail.com>
2022-08-17 18:47:59 -0300

    creacion base del search bar

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
-export default function SearchBar(){
-    return(
-        <div className={styles.container}>
-            <h1>Hola estas en SearchBar</h1>
-        </div>
-    )
+export default function SearchBar() {
+  const dispatch = useDispatch();
+  const [productName, setProductName] = useState("")
+
+  function handleOnClick() {
+    productName ? dispatch(getProductByName(productName)) : alert("No escribiste nada");
+  }
+
+  return (
+    <div className={styles.container}>
+      <Link>
+        <span>About</span>
+      </Link>
+
+      <h1>GAED.JM</h1>
+
+      <input type="text"
+        value={productName}
+        placeholder="Buscar..."
+        onChange={e => setProductName(e.target.value)} />
+
+      <button onClick={handleOnClick}>Buscar</button>
+
+      <Link>Carrito</Link>
+      <Link>Favoritos</Link>
+      <Link>Registrate</Link>
+      <Link>Inicia Sesion</Link>
+    </div>
+  )
 }
