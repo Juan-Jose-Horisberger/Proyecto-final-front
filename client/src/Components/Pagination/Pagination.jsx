f8455a2b4c8b4e6063604c619a307df96d03d218
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2022-08-18 14:25:36 -0300

    Modificaciones necesarias para el paginado

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Pagination.module.css';
+import Product from '../Product/Product.jsx';
 
-export default function Pagination(){
-    return(
-        <div className={styles.container}>
-            <h1>Hola estas en Pagination</h1>
+export default function Pagination({ allProducts, loaded }) {
+
+    return (
+
+        <div className={`${styles.container_Cards} col-9`} style={{ border: '1px solid red' }}>
+
+            {
+                loaded ? (
+                    <div className={`d-flex flex-wrap justify-content-sm-evenly`}>
+                        {allProducts.length ? allProducts.map((p, i) =>
+                            <Product
+                                key={i}
+                                id={p.id}
+                                name={p.nombre}
+                                price={p.precio}
+                                image={p.img}
+                            />
+                        )
+                            : <h1>No se encontro lo que se esta buscando</h1>
+                        }
+                    </div>
+                )
+                : <p>Loading...</p>
+            }
+
         </div>
+
     )
 }
