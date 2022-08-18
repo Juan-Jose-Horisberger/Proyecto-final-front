94245651c414fc1eaf2d4ef3494fae98792f8b65
EzequielZarcos <ezequielzarcos159@gmail.com>
2022-08-18 17:00:19 -0300

    funcionamiento de inputs, select y formulario

--- client/src/Components/FormProduct/useForm.js
+++ client/src/Components/FormProduct/useForm.js
@@ -1,16 +1,38 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
-export default function useForm (initialForm, validateForm) {
+export default function useForm(initialForm, validateForm) {
     const dispatch = useDispatch();
     const [form, setForm] = useState(initialForm);
     const [errors, setErrors] = useState({});
 
     const handleOnChange = (e) => {
-        setForm({
-            ...form, [e.target.name]: e.target.value
+        if (e.target.name === "category") {
+            if (e.target.value === "calzado") {
+                setForm({
+                    ...form, [e.target.name]: e.target.value, size: []
+                })
+            } else if (e.target.value !== "calzado") {
+                setForm({
+                    ...form, [e.target.name]: e.target.value, size: [...form.size.filter(e => e === "M" || e === "S" || e === "XS" || e === "L")]
+                })
+            };
+        } else {
+            setForm({
+                ...form, [e.target.name]: e.target.value
+            })
+        }
+    };
+
+    const handleChecked = (ev) => {
+        if (ev.target.checked) {
+            setForm({
+                ...form, size: [...form.size, ev.target.name]
+            })
+        } else setForm({
+            ...form, size: form.size.filter(e => e !== ev.target.name)
         })
-    }
+    };
 
     const handleSubmit = (e) => {
 
@@ -19,8 +41,10 @@ export default function useForm (initialForm, validateForm) {
 
     return {
         form,
+        setForm,
         errors,
         handleOnChange,
-        handleSubmit
+        handleSubmit,
+        handleChecked
     }
 }
