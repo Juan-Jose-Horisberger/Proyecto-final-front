6dc69aa311103f0d9145e1f766c42c004856209a
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2022-08-21 20:52:22 -0300

    Validaciones en contact + estilos, agregamos alerta success

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,105 +1,187 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './Contact.module.css';
 import { validate } from './validate.js';
 import SearchBar from '../SearchBar/SearchBar';
 import emailjs from '@emailjs/browser';
+import { Link } from 'react-router-dom';
+require('dotenv').config()
 
 export default function Contact() {
+    const form = useRef();
     const [input, setInput] = useState({
-        name: "",
-        email: "",
-        affair: "",
-        message: ""
+        user_name: "",
+        user_email: "",
+        user_affair: "",
+        user_message: ""
     })
 
     const [errorsExist, setErrorsExist] = useState(false); // Este estado local habilita el submit
-    const [error, setError] = useState({});
-    const [enableErrors, setEnableErrors] = useState(false);
+    const [error, setError] = useState({
+        user_name: "",
+        user_email: "",
+        user_affair: "",
+        user_message: ""
+    }); //Estado maneja errores
+    const [errorLocal, setErrorLocal] = useState({})
+    // const [enableErrors, setEnableErrors] = useState(false);
+    const [enableErrors, setEnableErrors] = useState({
+        name: false,
+        email: false,
+        affair: false,
+        message: false,
+    });
+
+
+
     const [notError, setNotError] = useState(false);
+    const [submittedForm, setSubmittedForm] = useState(false);
+    const [submittedFormTrue, setSubmittedFormTrue] = useState(false);
+    const [cleanForm, setCleanForm] = useState(false); //Este estado me permite que los inputs vuelvan con su borde inicial
 
 
     function handleOnChange(e) {
-        const objError = validate({ ...input, [e.target.name]: e.target.value });
-        setError(objError)
+        const objError = validate({ ...input, [e.target.name]: e.target.value }, e.target.name, error, setError);
 
         setInput({
             ...input,
             [e.target.name]: e.target.value
         })
-        if (e.target.value) {
-            setEnableErrors(true)
+        if (e.target.name === "user_name") {
+            setEnableErrors({
+                ...enableErrors,
+                name: true
+            })
+            setCleanForm(false);
+            return;
+        }
+        if (e.target.name === "user_email") {
+            setEnableErrors({
+                ...enableErrors,
+                email: true
+            })
+            setCleanForm(false);
+            return;
+        }
+        if (e.target.name === "user_affair") {
+            setEnableErrors({
+                ...enableErrors,
+                affair: true
+            })
+            setCleanForm(false);
+            return;
+        }
+        if (e.target.name === "user_message") {
+            setEnableErrors({
+                ...enableErrors,
+                message: true
+            })
+            setCleanForm(false);
+            return;
         }
     }
 
     function errorExist(e) {
         e.preventDefault();
-        // console.log( error.name, error.email)
+        // console.log( error.user_name, error.user_email)
+        console.log(
+            error.user_name,
+            error.user_email,
+            error.user_affair,
+
+        )
+
         if (
-            error.name ||
-            error.email ||
-            error.affair ||
-            error.message
+            error.user_name ||
+            error.user_email ||
+            error.user_affair ||
+            error.user_message
         ) {
             setErrorsExist(true);
             setNotError(false);
-            console.log("entra aca 1")
+            console.log("entro al if")
         }
-        else if (!input.name.length || !input.email.length || !input.affair.length) {
+        if (!input.user_name.length || !input.user_email.length || !input.user_affair.length || !input.user_message.length) {
             setErrorsExist(true);
             setNotError(false);
+            console.log('entro al else if')
         }
         else {
             setErrorsExist(false);
             setNotError(true);
+            console.log('entro al else')
         }
     }
 
     function validateErrors(e) {
         e.preventDefault();
         if (
-            error.name ||
-            error.email ||
-            error.affair ||
-            error.message
+            error.user_name ||
+            error.user_email ||
+            error.user_affair ||
+            error.user_message
         ) {
             setErrorsExist(true);
             setNotError(true);
         }
-        else if (!input.name.length || !input.email.length || input.affair.length || input.message.length) {
+        else if (!input.user_name.length || !input.user_email.length || input.user_affair.length || input.user_message.length) {
             setErrorsExist(true);
             setNotError(true);
         }
         setErrorsExist(false);
     }
 
+
     function handleSubmit(e) {
         e.preventDefault();
-        console.log('entra aca??')
         if (
-            !error.name ||
-            !error.email ||
-            !error.affair ||
-            !error.message
+            !error.user_name.length ||
+            !error.user_email.length ||
+            !error.user_affair.length ||
+            !error.user_message.length
         ) {
-            if (input.name && input.email && input.affair && input.message) {
-                emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, e.target, process.env.YOUR_PUBLIC_KEY).then(res => {
-                    alert("Se ha enviado correctamente.");
-                    console.log(res);
-                    setInput({
-                        name: "",
-                        email: "",
-                        affair: "",
-                        message: ''
+            if (input.user_name && input.user_email && input.user_affair && input.user_message) {
+                emailjs.sendForm(
+                    process.env.REACT_APP_YOUR_SERVICE_ID,
+                    process.env.REACT_APP_YOUR_TEMPLATE_ID,
+                    e.target,
+                    process.env.REACT_APP_YOUR_PUBLIC_KEY)
+                    .then(res => {
+                        setInput({
+                            user_name: "",
+                            user_email: "",
+                            user_affair: "",
+                            user_message: ''
+                        })
                     })
-                })
+                    .then(res => res === undefined && setSubmittedForm(true))
+                    .then(res => res === undefined && setCleanForm(true))
+                    .then(res => res === undefined && setEnableErrors({
+                        name: false,
+                        email: false,
+                        affair: false,
+                        message: false,
+                    }))
             }
         }
         //3:27
     }
 
+    function closeSuccessAlert(e) {
+        e.preventDefault();
+        setSubmittedFormTrue(true);
+    }
+
     return (
-        <div className={`container-fluid p-0`}>
+        <div className={`container-fluid p-0 ${styles.container}`}>
             <SearchBar />
+            <div className={`${styles.container_Inicio}`}>
+                <div>
+                    <Link to="/"><span>Inicio</span></Link>
+                    <span> / </span>
+                    <span>Contacto</span>
+                </div>
+
+            </div>
             <div className={`d-flex mb-2 col-12 ${styles.container_Info}`}>
                 <div className={`col-5 ${styles.container_image}`}>
                     <img
@@ -115,7 +197,7 @@ export default function Contact() {
                             <h2>Contactate</h2>
                             <p>¿Tenes alguna consulta, queres dejar tus productos en consignación o realizar un encargo? Contactate con nosotros.</p>
                             <div className={`d-flex mb-3`}>
-                                <img src="https://www.svgrepo.com/show/128093/location-pin.svg" width="23px" alt="" />
+                                <img src="https://www.svgrepo.com/show/423301/location.svg" width="23px" alt=""/>
                                 <p className={`m-0 ${styles.localInfop}`}>Olazabal 1515 Oficina 201 Cuerpo A, Belgrano,Cp 1428</p>
                             </div>
                             <div className={`d-flex`}>
@@ -124,69 +206,86 @@ export default function Contact() {
                             </div>
                         </div>
                     </div>
-                    <form onSubmit={(e) => handleSubmit(e)} className={`container-fluid d-flex justify-content-center col-12 p-0`} >
+                    <form ref={form} onSubmit={(e) => handleSubmit(e)} className={`container-fluid d-flex justify-content-center col-12 p-0`} >
                         <div className={`row col-12`}>
                             <div className='mb-3'>
                                 <input
                                     type="text"
                                     // className={`form-control ms-0 ${error.name ? 'is-invalid' : 'is-valid'}`}
-                                    className={`form-control ms-0 ${enableErrors && (error.name ? 'is-invalid' : 'is-valid')}`}
+                                    className={`
+                                    form-control 
+                                    ms-0 
+                                    ${styles.inputs}
+                                    ${(enableErrors.name && cleanForm === false) ? (error.user_name ? 'is-invalid' : 'is-valid') : 'form-control'}`}
                                     placeholder='Nombre *'
-                                    name='name'
-                                    value={input.name}
+                                    name='user_name'
+                                    value={input.user_name}
                                     autoComplete='off'
                                     onChange={(e) => handleOnChange(e)}
                                 />
                                 {
-                                    error.name && <p className="invalid-feedback mb-0">{error.name}</p>
+                                    error.user_name && <p className="invalid-feedback mb-0">{error.user_name}</p>
                                 }
                             </div>
                             <div className='mb-3'>
-
                                 <input
                                     type="text"
-                                    className={`form-control ms-0 ${enableErrors && (error.email ? 'is-invalid' : 'is-valid')}`}
+                                    className={`
+                                    form-control 
+                                    ms-0 
+                                    ${styles.inputs}
+                                    ${(enableErrors.email && cleanForm === false) ? (error.user_email ? 'is-invalid' : 'is-valid') : 'form-control'}`}
                                     placeholder='Email *'
-                                    name='email'
-                                    value={input.email}
+                                    name='user_email'
+                                    value={input.user_email}
                                     autoComplete='off'
                                     onChange={(e) => handleOnChange(e)}
+
                                 />
                                 {
-                                    error.email && <p className="invalid-feedback mb-0">{error.email}</p>
+                                    error.user_email && <p className="invalid-feedback mb-0">{error.user_email}</p>
                                 }
                             </div>
                             <div className='mb-3'>
                                 <input
                                     type="text"
-                                    className={`form-control ms-0 ${enableErrors && (error.affair ? 'is-invalid' : 'is-valid')}`}
+                                    className={`
+                                    form-control 
+                                    ms-0 
+                                    ${styles.inputs}
+                                    ${(enableErrors.affair && cleanForm === false) ? (error.user_affair ? 'is-invalid' : 'is-valid') : 'form-control'}`}
                                     placeholder='Asunto *'
-                                    name='affair'
-                                    value={input.affair}
+                                    name='user_affair'
+                                    value={input.user_affair}
                                     autoComplete='off'
                                     onChange={(e) => handleOnChange(e)}
+
                                 />
                                 {
-                                    error.affair && <p className="invalid-feedback mb-0">{error.affair}</p>
+                                    error.user_affair && <p className="invalid-feedback mb-0">{error.user_affair}</p>
                                 }
                             </div>
 
                             <div className={`${styles.container_TextArea}`} style={{ marginTop: "10px" }}>
                                 <textarea
-                                    name="message"
-                                    className={`form-control ${enableErrors && (error.message ? 'is-invalid' : 'is-valid')}`}
+                                    name="user_message"
+                                    className={`
+                                    form-control 
+                                    ${styles.inputs}
+                                    ${(enableErrors.message && cleanForm === false) ? (error.user_message ? 'is-invalid' : 'is-valid') : 'form-control'}`}
                                     autoComplete='off'
                                     placeholder='Mensaje *'
-                                    value={input.message}
+                                    value={input.user_message}
                                     onChange={(e) => handleOnChange(e)}
+                                    style={{resize: "none"}}
                                 ></textarea>
-                                <p className="invalid-feedback mb-0">{error.message}</p>
+                                <p className="invalid-feedback mb-0">{error.user_message}</p>
                             </div>
                             <div className={`${styles.container_button}`}>
                                 {
-                                    (errorsExist && !input.name.length || !input.email || !input.affair || !input.message)
-                                        ? (<button name="button" onClick={(e) => errorExist(e)}>ENVIAR MENSAJE</button>)
-                                        : (<button type="submit" name="button">ENVIAR MENSAJE</button>)
+                                    (errorsExist && !input.user_name.length || !input.user_email || !input.user_affair || !input.user_message)
+                                        ? (<button name="button" onClick={(e) => errorExist(e)} className={`${styles.button_send}`}>ENVIAR MENSAJE</button>)
+                                        : (<button type="submit" name="button" className={`${styles.button_send}`}>ENVIAR MENSAJE</button>)
                                 }
                             </div>
                             {/* {console.log(notError)} {(errorsExist && notError === false) ? styles.open : styles.container_Alert}*/}
@@ -212,6 +311,27 @@ export default function Contact() {
                                 <div className={`${styles.alertMsj}`}>Por favor complete los campos correspondientes</div>
                                 <button type="button" className="btn-close" onClick={(e) => validateErrors(e)}></button>
                             </div>
+
+                            <div
+                                className={`
+                                alert alert-success alert-dismissible fade show d-flex 
+                                ${styles.container_AlertSuccess}
+                                ${((submittedForm === true) && (submittedFormTrue === false)) ? styles.open : styles.container_AlertSuccess}`}
+                                role="alert">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    fill="currentColor"
+                                    className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2"
+                                    viewBox="0 0 16 16"
+                                    role="img">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                                </svg>
+                                <div>Gracias por tu mensaje, se envio correctamente.</div>
+                                <button type="button" className="btn-close" onClick={(e) => closeSuccessAlert(e)} aria-label="Close"></button>
+                            </div>
+
                         </div>
                     </form>
                 </div>
