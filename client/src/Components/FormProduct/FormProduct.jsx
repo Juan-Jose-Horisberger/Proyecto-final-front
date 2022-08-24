df7f0934727bbd596bf74086f3e9215a53d4c432
EzequielZarcos <ezequielzarcos159@gmail.com>
2022-08-24 20:34:51 -0300

    mejoro el formulario de creacion de producto

--- client/src/Components/FormProduct/FormProduct.jsx
+++ client/src/Components/FormProduct/FormProduct.jsx
@@ -10,16 +10,17 @@ export default function FormProduct() {
     name: "",
     brand: "",
     category: "",
-    price: "0",
-    stock: "0",
+    price: "",
+    stock: "",
     image: "",
-    sold: "0",
+    sold: "",
     size: [],
-    score: "0",
-    genre: ""
+    score: "",
+    genre: "",
+    offer: false
   }
 
-  const validateForm = (form, nameInput, setValidate, setErrors) => {
+  const validateForm = (form, nameInput, setValidate) => {
     let errors = {};
     let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 
@@ -32,81 +33,12 @@ export default function FormProduct() {
         errors.name = "El nombre del producto solo acepta letras y espacios.";
         setValidate({ ...validate, name: true });
 
-      }
-      else if (form.name === "") {
+      } else if (form.name === "") {
         errors.name = "El nombre del producto es requerido";
         setValidate({ ...validate, name: true });
 
       } else {
         setValidate({ ...validate, name: false });
-      };
-    };
-
-    if (nameInput.includes("size")) {
-      if (form.size.length === 0) {
-        errors.size = "Debes seleccionar minimo 1 talle";
-      }
-    };
-
-    if (nameInput.includes("price")) {
-      if (form.price > 150000) {
-        errors.price = "El precio excede el maximo permitido";
-        setValidate({ ...validate, price: true });
-
-      } else if (form.price < 0) {
-        errors.price = "El precio no puede ser menor a 0"
-        setValidate({ ...validate, price: true });
-
-      }
-      else if (form.price === "") {
-        errors.price = "El precio es requerido";
-        setValidate({ ...validate, price: true });
-
-      } else {
-        setValidate({ ...validate, price: false });
-
-      }
-    };
-
-    if (nameInput.includes("brand")) {
-      if (form.brand === "") {
-        errors.brand = "Debes seleccionar una marca";
-        setValidate({ ...validate, brand: true });
-
-      } else {
-        setValidate({ ...validate, brand: false });
-
-      }
-    };
-
-    if (nameInput.includes("stock")) {
-      if (form.stock < 0) {
-        errors.stock = "El disponible no puede ser menor a 0";
-        setValidate({ ...validate, stock: true });
-
-      }
-      else if (form.stock === "") {
-        errors.stock = "El disponible es requerido";
-        setValidate({ ...validate, stock: true });
-
-      } else {
-        setValidate({ ...validate, stock: false });
-
-      };
-    };
-
-    if (nameInput.includes("sold")) {
-      if (form.sold < 0) {
-        errors.sold = "El vendido no puede ser menor a 0";
-        setValidate({ ...validate, sold: true });
-
-      }
-      else if (form.sold < 0) {
-        errors.sold = "La cantidad vendida es requerida";
-        setValidate({ ...validate, sold: true });
-
-      } else {
-        setValidate({ ...validate, sold: false });
 
       };
     };
@@ -122,13 +54,101 @@ export default function FormProduct() {
       };
     };
 
+    if (nameInput.includes("brand")) {
+      if (form.brand === "") {
+        errors.brand = "Debes seleccionar una marca";
+        setValidate({ ...validate, brand: true });
+
+      } else {
+        setValidate({ ...validate, brand: false });
+
+      };
+    };
+
+    if (nameInput.includes("image")) {
+      if (form.image === "") {
+        errors.image = "El producto necesita una imagen";
+        setValidate({ ...validate, image: true });
+
+      } else {
+        setValidate({ ...validate, image: false });
+
+      }
+    };
+
+    if (nameInput.includes("price")) {
+      if (form.price > 150000) {
+        errors.price = "El precio excede el maximo permitido";
+        setValidate({ ...validate, price: true });
+
+      } else if (form.price < 0) {
+        errors.price = "El precio no puede ser menor a 0"
+        setValidate({ ...validate, price: true });
+
+      } else if (form.price === "") {
+        errors.price = "El precio es requerido";
+        setValidate({ ...validate, price: true });
+
+      } else {
+        setValidate({ ...validate, price: false });
+
+      };
+    };
+
+    if (nameInput.includes("stock")) {
+      if (form.stock < 0) {
+        errors.stock = "El disponible no puede ser menor a 0";
+        setValidate({ ...validate, stock: true });
+
+      } else if (form.stock === "") {
+        errors.stock = "El disponible es requerido";
+        setValidate({ ...validate, stock: true });
+
+      } else {
+        setValidate({ ...validate, stock: false });
+
+      };
+    };
+
+    if (nameInput.includes("sold")) {
+      if (form.sold < 0) {
+        errors.sold = "El vendido no puede ser menor a 0";
+        setValidate({ ...validate, sold: true });
+
+      } else if (form.sold === "") {
+        errors.sold = "La cantidad vendida es requerida";
+        setValidate({ ...validate, sold: true });
+
+      } else {
+        setValidate({ ...validate, sold: false });
+
+      };
+    };
+
+    if (nameInput.includes("size")) {
+      if (form.size.length === 0) {
+        errors.size = "Debes seleccionar minimo 1 talle";
+
+      };
+    };
+
+    if (nameInput.includes("genre")) {
+      if (form.genre === "") {
+        errors.genre = "El producto debe pertener a un genero"
+        setValidate({ ...validate, genre: true });
+
+      } else {
+        setValidate({ ...validate, genre: false });
+
+      }
+    };
+
     if (nameInput.includes("score")) {
       if (form.score > 5) {
         errors.score = "La puntuacion excede el maximo permitido";
         setValidate({ ...validate, score: true });
 
-      }
-      else if (form.score < 0) {
+      } else if (form.score < 0) {
         errors.score = "La puntuacion excede el minimo permitido";
         setValidate({ ...validate, score: true });
 
@@ -143,46 +163,21 @@ export default function FormProduct() {
       };
     };
 
-    if (nameInput.includes("genre")) {
-      if (form.genre === "") {
-        errors.genre = "El producto debe pertener a un genero"
-        setValidate({ ...validate, genre: true });
-
-      } else {
-        setValidate({ ...validate, price: false });
-
-      }
-    };
-
-    if (nameInput.includes("image")) {
-      if (form.image === "") {
-        errors.image = "El producto necesita una imagen";
-        setValidate({ ...validate, image: true });
-
-      } else {
-        setValidate({ ...validate, image: false });
-
-      }
-    };
-
     return errors;
   };
 
   const {
     form,
-    button,
     errors,
     handleOnChange,
     handleSubmit,
     handleChecked,
-    handleOnButton,
     handleOnSubmit,
     uploadImage,
-    image,
     validate,
-    setValidate,
     alert,
-    setAlert
+    setAlert,
+    handleOffer
   } = useForm(initialForm, validateForm);
 
 
@@ -192,48 +187,48 @@ export default function FormProduct() {
         <button type="button" className="btn-close btn-close-white" aria-label="Close"></button>
       </Link>
 
-      <h2>Agrega un producto al catalogo</h2>
-
       <div className={style.container}>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="name">Nombre</label>
             <input type="text"
               className={`form-control ${validate.name ? "is-invalid" : (validate.name !== false ? "" : "is-valid")}`}
               name="name"
               id="name"
               value={form.name}
+              placeholder="Nombre del producto..."
               onChange={handleOnChange} />
             {errors.name && <p className={style.error}>{errors.name}</p>}
           </div>
 
-          <div className={style.catbrand}>
-            <label htmlFor="category">Categoria</label>
-            <select name="category" onChange={handleOnChange} >
-              <option style={{ display: "none" }} >Category</option>
-              <option value="calzado" >Calzado</option>
-              <option value="camiseta" >Camiseta</option>
-              <option value="pantalon" >Pantalon</option>
-              <option value="buzo" >Buzo</option>
-              <option value="campera" >Campera</option>
-            </select>
+          <div className={style.categoryBrand}>
 
-            <label htmlFor="brand">Marca</label>
-            <select name="brand" onChange={handleOnChange} >
-              <option style={{ display: "none" }} >Brand</option>
-              <option value="Adidas" >Adidas</option>
-              <option value="Nike" >Nike</option>
-              <option value="Puma" >Puma</option>
-            </select>
+            <div>
+              <label htmlFor="category">Categoria</label>
+              <select name="category" onChange={handleOnChange} id="my_select" className={`form-control ${validate.category ? "is-invalid" : (validate.category !== false ? "" : "is-valid")}`} >
+                <option style={{ display: "none" }} >Categoria</option>
+                <option value="calzado" >Calzado</option>
+                <option value="camiseta" >Camiseta</option>
+                <option value="pantalon" >Pantalon</option>
+                <option value="buzo" >Buzo</option>
+                <option value="campera" >Campera</option>
+              </select>
+            </div>
 
+            <div>
+              <label htmlFor="brand">Marca</label>
+              <select name="brand" onChange={handleOnChange} id="my_select" className={`form-control ${validate.brand ? "is-invalid" : (validate.brand !== false ? "" : "is-valid")}`}>
+                <option style={{ display: "none" }} >Marca</option>
+                <option value="Adidas" >Adidas</option>
+                <option value="Nike" >Nike</option>
+                <option value="Puma" >Puma</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
             {errors.category && <p className={style.error}>{errors.category}</p>}
             {errors.brand && <p className={style.error}>{errors.brand}</p>}
           </div>
-
-          {/* <button type="button"
-            className="btn btn-primary"
-            onClick={handleOnButton} > {button} </button>
-          <div className={style.divImage}> */}
 
           <div className="input-group mb-3">
             <input type="file"
@@ -243,66 +238,54 @@ export default function FormProduct() {
               placeholder="asdasd">
             </input>
           </div>
-          {/* {button === "URL" ?
-              <div><label htmlFor="image">Imagen</label>
-                <input type="text"
-                  name="image"
-                  id="image"
-                  value={form.image}
-                  onChange={handleOnChange}
-                /> </div>
 
-              :
-
-              <div>
-                <label htmlFor="image" cl>Imagen</label>
-                <input type="file"
-                  className={style.inputFile}
-                  name="image"
-                  id="image"
-                  value={form.image}
-                  onChange={handleOnChange}
-                  accept="image/*"
-                /> </div>} */}
-          {errors.image && <p className={style.error}>{errors.image}</p>}
-          {/* </div> */}
-
-          {!errors.image &&
+          {errors.image ?
+            <p className={style.error}>{errors.image}</p>
+            :
+            !errors.image &&
             <div>
-              <img src={image} alt="Image not found" className={style.imagen} />
+              <img src={form.image} alt="Image not found" className={style.imagen} />
             </div>}
 
-          <div className={style.divPrice}>
-            <label htmlFor="price">Precio</label>
-            <input type="number"
-              className={`form-control ${validate.price ? "is-invalid" : (validate.price !== false ? "" : "is-valid")}`}
-              name="price"
-              id="price"
-              value={form.price}
-              onChange={handleOnChange} />
+          <div className={style.stockPriceVendido}>
+            <div className={style.divStockPriceSold}>
+              <label htmlFor="price">Precio</label>
+              <input type="number"
+                className={`form-control ${validate.price ? "is-invalid" : (validate.price !== false ? "" : "is-valid")}`}
+                name="price"
+                id="price"
+                placeholder="00.000"
+                value={form.price}
+                onChange={handleOnChange} />
+            </div>
 
-            {errors.price && <p className={style.error}>{errors.price}</p>}
+            <div className={style.divStockPriceSold}>
+              <label htmlFor="stock">Disponible</label>
+              <input type="number"
+                className={`form-control ${validate.stock ? "is-invalid" : (validate.stock !== false ? "" : "is-valid")}`}
+                name="stock"
+                id="stock"
+                min="0"
+                placeholder="0"
+                value={form.stock}
+                onChange={handleOnChange} />
+            </div>
+
+            <div className={style.divStockPriceSold}>
+              <label htmlFor="sold">Vendidos</label>
+              <input type="number"
+                className={`form-control ${validate.sold ? "is-invalid" : (validate.sold !== false ? "" : "is-valid")}`}
+                name="sold"
+                id="sold"
+                min="0"
+                placeholder="0"
+                value={form.sold}
+                onChange={handleOnChange} />
+            </div>
           </div>
 
-          <div className={style.stockVendido}>
-            <label htmlFor="stock">Disponible</label>
-            <input type="number"
-              className={`form-control ${validate.stock ? "is-invalid" : (validate.stock !== false ? "" : "is-valid")}`}
-              name="stock"
-              id="stock"
-              min="0"
-              value={form.stock}
-              onChange={handleOnChange} />
-
-            <label htmlFor="sold">Vendidos</label>
-            <input type="number"
-              className={`form-control ${validate.sold ? "is-invalid" : (validate.sold !== false ? "" : "is-valid")}`}
-              name="sold"
-              id="sold"
-              min="0"
-              value={form.sold}
-              onChange={handleOnChange} />
-
+          <div>
+            {errors.price && <p className={style.error}>{errors.price}</p>}
             {errors.stock && <p className={style.error}>{errors.stock}</p>}
             {errors.sold && <p className={style.error}>{errors.sold}</p>}
           </div>
@@ -352,25 +335,10 @@ export default function FormProduct() {
             {errors.size && <p className={style.error}>{errors.size}</p>}
           </div>
 
-          <div className={style.calificacion}>
-            <label htmlFor="score">Calificacion</label>
-            <input type="number"
-              className={`form-control ${validate.score ? "is-invalid" : (validate.score !== false ? "" : "is-valid")}`}
-              name="score"
-              min="0"
-              max="5"
-              step="0.1"
-              id="score"
-              value={form.score}
-              onChange={handleOnChange} />
-
-            {errors.score && <p className={style.error}>{errors.score}</p>}
-          </div>
-
-          <div className={style.genre}>
+          <div className={style.divGenre}>
             <label htmlFor="genre">Genero</label>
-            <select name="genre" onChange={handleOnChange} className={`form-control ${validate.genre ? "is-invalid" : (validate.genre !== false ? "" : "is-valid")}`}>
-              <option style={{ display: "none" }}>Genre</option>
+            <select name="genre" onChange={handleOnChange} id="my_select" className={`form-control ${(validate.genre) ? "is-invalid" : (validate.genre !== false ? "" : "is-valid")}`}>
+              <option style={{ display: "none" }}>Genero</option>
               <option value="hombre" >Hombre</option>
               <option value="mujer" >Mujer</option>
             </select>
@@ -378,12 +346,37 @@ export default function FormProduct() {
             {errors.genre && <p className={style.error}>{errors.genre}</p>}
           </div>
 
+          <div className={style.divScore}>
+            <label htmlFor="score">Calificacion</label>
+            <input type="number"
+              className={`form-control ${(validate.score) ? `is-invalid` : (validate.score !== false ? "" : "is-valid")}`}
+              name="score"
+              min="0"
+              max="5"
+              step="0.1"
+              id="score"
+              placeholder="0.0"
+              value={form.score}
+              onChange={handleOnChange} />
+
+            {errors.score && <p className={style.error}>{errors.score}</p>}
+          </div>
+
+          <div>
+            <label className="form-check-label">En oferta</label>
+            <input type="checkbox"
+              className="form-check-input"
+              name="size"
+              value={form.offer}
+              onChange={handleOffer} />
+          </div>
+
           <button onSubmit={handleOnSubmit}
             className="btn btn-success m-3"
           >Sumar al catalogo</button>
 
           {alert ?
-            <div className="alert alert-danger alert-dismissible fade show" role="alert">
+            <div className={`${alert ? style.container_AlertDangerOpen : style.container_AlertDanger} alert alert-danger alert-dismissible fade show`}>
               Debes completar el formulario con la informacion <strong>correcta!</strong>
               <button type="button" className="btn-close" onClick={() => setAlert()} aria-label="Close"></button>
             </div>
@@ -392,11 +385,10 @@ export default function FormProduct() {
               alert !== false ?
                 <div></div>
                 :
-                <div className="alert alert-success alert-dismissible fade show" role="alert">
+                <div className={`${alert ? style.container_AlertSuccess : style.container_AlertSuccessOpen} alert alert-success alert-dismissible fade show`} role="alert">
                   El producto se ha <strong>sumado al cataglogo</strong> con exito!
                   <button type="button" className="btn-close" onClick={() => setAlert()} aria-label="Close"></button>
                 </div>)}
-
         </form>
       </div>
     </div>
