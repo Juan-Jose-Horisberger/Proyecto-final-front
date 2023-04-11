42bec23eebe30c1b61e2e55a3b72bd9fca255c15
FrancoMED <francoemedina@gmail.com>
2023-04-10 21:19:43 -0300

    route back deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home.jsx';
 import ProductDetail from './Components/ProductDetail/ProductDetail.jsx';
 import FavoriteProduct from './Components/FavoriteProduct/FavoriteProduct.jsx';
 import Cart from './Components/Cart/Cart.jsx';
-import Login from './Components/Login/Login.jsx';
+// import Login from './Components/Login/Login.jsx';
 import Register from './Components/Register/Register.jsx';
 import FormProduct from './Components/FormProduct/FormProduct.jsx';
 import Contact from './Components/Contact/Contact.jsx';
 import Profile from './Components/Profile/Profile.jsx';
 import Logout from './Components/Logout/Logout';
 import Dashboard from './Components/Dashboard/Dashboard.jsx';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 import Checkout from './Components/Checkout/Checkout.jsx';
-import { posts } from "./infoUser.js";
+import { posts } from './infoUser.js';
 // import Navbar from './Components/NavbarPrueba/NavBar.jsx';
 // import Card from './Components/CardPrueba/Card.jsx';
 // import "./app.css";
 
 function App() {
-  const [socket, setSocket] = useState(null);
-  useEffect(() => {
-    setSocket(io("http://localhost:5000")) //Inicializamos la conexion con el servidor socket.
-  }, []);
+	const [socket, setSocket] = useState(null);
+	useEffect(() => {
+		setSocket(io('http://localhost:5000')); //Inicializamos la conexion con el servidor socket.
+	}, []);
 
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home socket={socket} />} />
-        <Route path="/ProductDetail/:id" element={<ProductDetail />} />
-        <Route path="/FavoriteProduct" element={<FavoriteProduct />} />
-        <Route path="/Cart" element={<Cart />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/Profile" element={<Profile />} />
-        <Route path="/Logout" element={<Logout />} />
-        <Route path="/Register" element={<Register socket={socket} />} />
-        <Route path="/CreateProduct" element={<FormProduct />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Dashboard" element={<Dashboard socket={socket} />}/>
-        <Route path="/Checkout" element={<Checkout socket={socket} />} />
-        <Route path="/Profile" element={<Profile />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home socket={socket} />} />
+				<Route path="/ProductDetail/:id" element={<ProductDetail />} />
+				<Route path="/FavoriteProduct" element={<FavoriteProduct />} />
+				<Route path="/Cart" element={<Cart />} />
+				{/* <Route path="/Login" element={<Login />} /> */}
+				<Route path="/Profile" element={<Profile />} />
+				<Route path="/Logout" element={<Logout />} />
+				<Route path="/Register" element={<Register socket={socket} />} />
+				<Route path="/CreateProduct" element={<FormProduct />} />
+				<Route path="/Contact" element={<Contact />} />
+				<Route path="/Dashboard" element={<Dashboard socket={socket} />} />
+				<Route path="/Checkout" element={<Checkout socket={socket} />} />
+				<Route path="/Profile" element={<Profile />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
